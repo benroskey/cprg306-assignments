60773c31bc7d3cd1e120a70e9d5c831dbee4f9db
Benjamin Roskey <benjamin.roskey@edu.sait.ca>
2023-09-21 16:14:38 -0600

    Page + Student Info
    edit home page, added important import to top of student info page

--- app/StudentInfo.jsx
+++ app/StudentInfo.jsx
@@ -1,10 +1,12 @@
-export function StudentInfo() {
+import Link from 'next/link';
+
+export default function StudentInfo() {
     return (
         <div>
             <h1>Student Information</h1>
             <p>Student Name: Benjamin Roskey</p>
             <p>CPRG 306 B SA1</p>
-            <nav> <Link href ="https://github.com/benroskey"> My Github</Link> </nav>
+            <Link href ="https://github.com/benroskey"> My Github </Link>
         </div>
     )
 }
