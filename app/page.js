a8f3583f443441c2053ad1c5814613ee9a88f464
Benjamin Roskey <benjamin.roskey@edu.sait.ca>
2023-09-21 16:27:13 -0600

    Week2 FOlder
    fixed errors, added week2 folder, page and correct information to it

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import StudentInfo from './StudentInfo.jsx';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
-      
+      <Link href ="/week2"> Week 2 </Link>
     </div>
   );  
 }
