0943d2c9c54d1a5b250340225c7078e48c148632
Benjamin Roskey <benjamin.roskey@edu.sait.ca>
2023-10-13 15:16:35 -0600

    used wrong "array"
    -switched to .json file
    -correctly imported the .json array

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -2,96 +2,12 @@
 
 import { useState } from "react";
 import Item from "./item.js";
+import itemsData from "./items.json";
 
 export default function ItemList() {
   const [sortby, setSortBy] = useState("name");
 
-  const item1 = {
-    name: "Milk, 4 L 🥛",
-    quantity: 1,
-    category: "dairy",
-  };
-
-  const item2 = {
-    name: "Bread 🍞",
-    quantity: 2,
-    category: "bakery",
-  };
-
-  const item3 = {
-    name: "eggs, dozen 🥚",
-    quantity: 2,
-    category: "dairy",
-  };
-
-  const item4 = {
-    name: "bananas 🍌",
-    quantity: 6,
-    category: "produce",
-  };
-
-  const item5 = {
-    name: "broccoli 🥦",
-    quantity: 3,
-    category: "produce",
-  };
-
-  const item6 = {
-    name: "chicken breasts, 1 kg 🍗",
-    quantity: 1,
-    category: "meat",
-  };
-
-  const item7 = {
-    name: "pasta sauce 🍝",
-    quantity: 3,
-    category: "canned goods",
-  };
-
-  const item8 = {
-    name: "spaghetti, 454 g 🍝",
-    quantity: 2,
-    category: "dry goods",
-  };
-
-  const item9 = {
-    name: "toilet paper, 12 pack 🧻",
-    quantity: 1,
-    category: "household",
-  };
-
-  const item10 = {
-    name: "paper towels, 6 pack",
-    quantity: 1,
-    category: "household",
-  };
-
-  const item11 = {
-    name: "dish soap 🍽️",
-    quantity: 1,
-    category: "household",
-  };
-
-  const item12 = {
-    name: "hand soap 🧼",
-    quantity: 4,
-    category: "household",
-  };
-
-  let array = [
-    item1,
-    item2,
-    item3,
-    item4,
-    item5,
-    item6,
-    item7,
-    item8,
-    item9,
-    item10,
-    item11,
-    item12,
-  ];
+  let array = itemsData;
 
   //below returns the javascript
   let sortedarray = array.sort((a, b) => {
--- app/week5/page.js
+++ app/week5/page.js
@@ -6,7 +6,7 @@ export default function Page(){
     <main>
         <div>
             <Link href ="/"> <h1 className="text-2xl font-bold m-5">Home</h1> </Link>
-            <h1 className= "text-5xl font-bold m-2">Shopping List</h1>        
+            <h1 className= "text-5xl font-bold m-2">Shopping List</h1>
             <ItemList />
         </div>
     </main>
