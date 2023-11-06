a1d35458efc060ff84721267fa64630678f69c03
Benjamin Roskey <benjamin.roskey@edu.sait.ca>
2023-11-06 14:02:31 -0700

    added week8 link

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
         <Link href ="/week5"> Week 5 </Link> <br/>
         <Link href ="/week6"> Week 6 </Link> <br/>
         <Link href ="/week7"> Week 7 </Link> <br/>
+        <Link href ="/week8"> Week 8 </Link> <br/>
       </div>
     </main>
   );
